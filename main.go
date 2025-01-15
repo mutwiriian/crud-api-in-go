d4a3d8e5a6c9e19340d4558bbf0f67b5931f0673
mutwiriian <mutwiri_ian@yahoo.com>
2025-01-15 20:29:28 +0300

    make port value exportable

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"log/slog"
 	"net/http"
-	"os"
 
 	"github.com/mutwiriian/crud-api-in-go/database"
 	"github.com/mutwiriian/crud-api-in-go/handlers"
@@ -18,8 +16,6 @@ func init() {
 }
 
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-
 	r := http.NewServeMux()
 
 	r.HandleFunc("GET /customers/get_all", handlers.GetCustomersHandler(db))
@@ -28,6 +24,5 @@ func main() {
 	r.HandleFunc("PATCH /customers/update", handlers.UpdateCustomerByEmailHandler(db))
 	r.HandleFunc("DELETE /customers/delete_email", handlers.DeleteCustomerByEmailHandler(db))
 
-	logger.Info("Starting server at ", "port", *database.Port)
 	http.ListenAndServe(":8000", r)
 }
