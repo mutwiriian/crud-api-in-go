65e7f21fd08b7dcf22458f7b84715f9079d9e44b
mutwiriian <mutwiri_ian@yahoo.com>
2025-01-27 21:32:10 +0300

    return from handlers on error

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,7 +17,6 @@ func (app *Application) internalServerErrorHandler(w http.ResponseWriter, err er
 	if err != nil {
 		http.Error(w, err.Error(), code)
 		app.Logger.Error(err.Error())
-		return
 	}
 }
 
@@ -39,12 +38,17 @@ func (app *Application) CreateCustomerHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		insertStmt, err := db.Prepare("insert into customers (name, email, phone_number, address) values ($1, $2, $3, $4)")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		defer insertStmt.Close()
 
 		_, err = insertStmt.Exec(customerPayload.Name, customerPayload.Email, customerPayload.Phone_number, customerPayload.Address)
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
@@ -58,12 +62,18 @@ func (app *Application) CreateCustomerHandler(db *sql.DB) http.HandlerFunc {
 func (app *Application) GetCustomersHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		getCustomersStmt, err := db.Prepare("select * from customers")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		defer getCustomersStmt.Close()
 
 		rows, err := getCustomersStmt.Query()
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		defer rows.Close()
 
@@ -73,13 +83,19 @@ func (app *Application) GetCustomersHandler(db *sql.DB) http.HandlerFunc {
 			var customer models.Customer
 
 			err := rows.Scan(&customer.Customer_id, &customer.Name, &customer.Email, &customer.Phone_number, &customer.Address)
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			customers = append(customers, customer)
 		}
 
 		err = rows.Err()
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
@@ -103,7 +119,10 @@ func (app *Application) GetCustomerByEmailHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		searchStmt, err := db.Prepare("select * from customers where email = $1")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		defer searchStmt.Close()
 
@@ -112,8 +131,9 @@ func (app *Application) GetCustomerByEmailHandler(db *sql.DB) http.HandlerFunc {
 		var customer models.Customer
 
 		err = row.Scan(&customer.Customer_id, &customer.Name, &customer.Email, &customer.Address, &customer.Address)
-		if err != nil {
+		if err != nil || err.Error() == " no rows in result set" {
 			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
@@ -135,10 +155,16 @@ func (app *Application) UpdateCustomerByEmailHandler(db *sql.DB) http.HandlerFun
 		var customerPayload models.UpdateCustomerSchema
 
 		err := json.NewDecoder(r.Body).Decode(&customerPayload)
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		searchStmt, err := db.Prepare("select * from customers where email = $1")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		defer searchStmt.Close()
 
@@ -146,47 +172,73 @@ func (app *Application) UpdateCustomerByEmailHandler(db *sql.DB) http.HandlerFun
 
 		row := searchStmt.QueryRow(customerEmail)
 		err = row.Scan(&customer.Customer_id, &customer.Name, &customer.Email, &customer.Phone_number, &customer.Address)
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		if customerPayload.Name == "" || customerPayload.Phone_number == "" || customerPayload.Address == "" {
 			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
 		}
 
 		tx, err := db.Begin()
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		if customerPayload.Name != "" {
 			updateNameStmt, err := tx.Prepare("update customers set name =$1 where email=$2")
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			_, err = updateNameStmt.Exec(customerPayload.Name, customerEmail)
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			defer updateNameStmt.Close()
 		}
 
 		if customerPayload.Phone_number != "" {
 			updatePhoneNumberStmt, err := tx.Prepare("update customers set phone_number")
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			_, err = updatePhoneNumberStmt.Exec(customerPayload.Phone_number, customerEmail)
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			defer updatePhoneNumberStmt.Close()
 		}
 
 		if customerPayload.Address != "" {
 			updateAddressStmt, err := tx.Prepare("update customers set address =$1 where email=$2")
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			_, err = updateAddressStmt.Exec(customerPayload.Address, customerEmail)
-			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			if err != nil {
+				app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+				return
+			}
 
 			defer updateAddressStmt.Close()
 		}
 
 		if err := tx.Commit(); err != nil {
 			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
@@ -207,22 +259,37 @@ func (app *Application) DeleteCustomerByEmailHandler(db *sql.DB) http.HandlerFun
 		customerEmail := r.URL.Query().Get("email")
 
 		searchStmt, err := db.Prepare("select * from customers where email = $1")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		var customer models.Customer
 
 		row := searchStmt.QueryRow(customerEmail)
 		err = row.Scan(&customer.Customer_id, &customer.Name, &customer.Email, &customer.Phone_number, &customer.Address)
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		deleteStmt, err := db.Prepare("delete from customers where email = $1")
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		res, err := deleteStmt.Exec(customerEmail)
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		affected, err := res.RowsAffected()
-		app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+		if err != nil {
+			app.internalServerErrorHandler(w, err, http.StatusInternalServerError)
+			return
+		}
 
 		if affected == 0 {
 			msg := "Now rows affected!"
